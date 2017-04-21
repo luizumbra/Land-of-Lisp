423edbfd7330e78466a61282487e44490dab9aca
luizumbra <luiz_fantasma@hotmail.com>
2017-04-20 23:37:43 -0300

    m

--- grand-theft-wumpus.lisp
+++ grand-theft-wumpus.lisp
@@ -268,3 +268,224 @@
   ;; ======
   ;;  Tie all of our island prevention functions together
   (append (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+
+(defun make-city-edges ()
+  ;; Function
+  ;; ========
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  Create the edges of Congetion-City with cops
+  (let* ((nodes (loop for i from 1 to *node-num*
+		      collect i))
+	 ;; Local-parameter
+	 ;; ===============
+	 ;;
+	 ;; Description:
+	 ;; ===========
+	 ;;  List of nodes
+	 (edge-list (connect-all-islands nodes (make-edge-list)))
+	 ;; Local-parameter
+	 ;; ===============
+	 ;;
+	 ;; Description:
+	 ;; ===========
+	 ;;  Creates a random (but fully connected) edge list
+	 (cops
+	  ;; Local-parameter
+	  ;; ===============
+	  ;;
+	  ;; Description
+	  ;; ===========
+	  ;;  Creates a random list of edges that contains cops
+	  (remove-if-not (lambda (x)
+			   ;; Lambda-function
+			   ;; ===============
+			   ;;
+			   ;; Input:
+			   ;; =====
+			   ;;  [1] - x, a edge
+			   ;;
+			   ;; Output:
+			   ;; ======
+			   ;;  Choose if x can have a cop-odd or not.
+			   (zerop (random *cop-odds*)))
+			 edge-list)))
+    (add-cops (edges-to-alist edge-list) cops)))
+
+(defun edges-to-alist (edge-list)
+  ;; Function
+  ;; ========
+  ;;
+  ;; Input:
+  ;; =====
+  ;;  [1] - edge-list, a list of edges of Congetion-City
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  Converts a list of edges into an alist of edges
+  ;;
+  ;; Example:
+  ;; =======
+  ;;  [1] > (edges-to-alist '((1 . 2) (2 . 1) (2 . 3) (3 . 2)))
+  ;;      > '((1 (2)) (2 (1) (3)) (3 (2)))
+  (mapcar (lambda (node1)
+	    ;; Lambda-function
+	    ;; ===============
+	    ;;
+	    ;; Input:
+	    ;; =====
+	    ;;  [1] - node1, A node to remove duplicates
+	    ;;
+	    ;; Output:
+	    ;; ======
+	    ;;  Nested mapcar functions allow edges-to-alist to convert the edges
+	    ;; of a city into an alist.
+            (cons node1
+                  (mapcar (lambda (edge)
+			    ;; Lambda-function
+			    ;; ===============
+			    ;;
+			    ;; Input:
+			    ;; =====
+			    ;;  [1] - edge,
+			    ;;
+			    ;; Output:
+			    ;; ======
+			    ;;  To build the list of nodes, we use the
+			    ;; remove-duplicates function, which removes
+			    ;; duplicate items from a list.
+			    (list (cdr edge)))
+			  (remove-duplicates (direct-edges node1 edge-list)
+					     :test #'equal))))
+	  (remove-duplicates (mapcar #'car edge-list))))
+
+(defun add-cops (edge-alist edges-with-cops)
+  ;; Function
+  ;; ========
+  ;;
+  ;; Input:
+  ;; =====
+  ;;  [1] - edge-alist, all edges of the city
+  ;;  [2] - edge-with-cops, the edges marked with cops-odds
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  Use this list of cop edges to mark the edges in our alist that contain cops
+  (mapcar (lambda (x)
+	    ;; Lambda-function
+	    ;; ===============
+	    ;;
+	    ;; Input:
+	    ;; =====
+	    ;;  [1] - x, a simple edge with format (a (b) (c))
+	    ;;
+	    ;; Output:
+	    ;; ======
+	    ;;  Nested mapcar commands to map across the edges within each node
+	    (let ((node1
+		   ;; Local-parameter
+		   ;; ===============
+		   ;;
+		   ;; Description:
+		   ;; ===========
+		   ;;  Knowing that x have the format (a (b) (c)), node1 is a
+		   (car x))
+		  (node1-edges
+		   ;; Local-parameter
+		   ;; ===============
+		   ;;
+		   ;; Description:
+		   ;; ===========
+		   ;;  Knowing that x have the format (a (b) (c)), node1 is
+		   ;; ((b) (c))
+		   (cdr x)))
+	      (cons node1
+		    (mapcar (lambda (edge)
+			      ;; Lambda-function
+			      ;; ===============
+			      ;;
+			      ;; Input:
+			      ;; =====
+			      ;;  [1] - edge, the edges of the city
+			      ;;
+			      ;; Output:
+			      ;; ======
+			      ;;  We then check whether there are any cops on a
+			      ;; given edge, using the intersection function.
+			      (let ((node2
+				     ;; Local-parameter
+				     ;; ===============
+				     ;;
+				     ;; Description:
+				     ;; ===========
+				     ;;  The first node of edge
+				     (car edge)))
+				(if (intersection (edge-pair node1 node2)
+						  edges-with-cops
+						  :test #'equal)
+				    (list node2 'cops)
+				  edge)))
+			    node1-edges))))
+	  edge-alist))
+
+(defun neighbors (node edge-alist)
+  ;; Function
+  ;; ========
+  ;;
+  ;; Input:
+  ;; =====
+  ;;  [1] - node, a simple node
+  ;;  [2] - edge-alist, alist of edges
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  Looks up the node’s neighbors using the alist of edges.
+  ;; If the second node is in that list, we know we’re one away.
+  (mapcar #'car (cdr (assoc node edge-alist))))
+
+
+(defun within-one (a b edge-alist)
+  ;; Function
+  ;; ========
+  ;;
+  ;; Input:
+  ;; =====
+  ;;  [1] - a,
+  ;;  [2] - b,
+  ;;  [3] - edge-alist,
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  Looks up the first node (a) in the alist of edges with
+  ;; neighbors. Then it uses member to see if the other node
+  ;; (b) is among these nodes.
+  (member b (neighbors a edge-alist)))
+
+(defun within-two (a b edge-alist)
+  ;; Function
+  ;; ========
+  ;;
+  ;; Input:
+  ;; =====
+  ;;  [1] - a,
+  ;;  [2] - b,
+  ;;  [3] - edge-alist,
+  ;;
+  ;; Output:
+  ;; ======
+  ;;  
+  (or (within-one a b edge-alist)
+      (some (lambda (x)
+	      ;; Lambda-function
+	      ;; ===============
+	      ;;
+	      ;; Input:
+	      ;; =====
+	      ;;  [1] - x,
+	      ;;
+	      ;; Output:
+	      ;; ======
+	      ;;  
+              (within-one x b edge-alist))
+            (neighbors a edge-alist))))
